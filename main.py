6948668937ac92beb1e113d07ac940254a9e51bc
Nakuveeran <70315348+Nakuveeran@users.noreply.github.com>
2020-09-03 14:34:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(n):
+    # Use a breakpoint in the code line below to debug your script.
+  #  print('***')  # Press Ctrl+F8 to toggle the breakpoint.
+    count =0
+    while(count<=9):
+      print('Akash Paveen Don')
+      count = count+1
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi(10)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
